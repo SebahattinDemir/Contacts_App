8956602cd5e9534de39bbf7c053a1fde87526b94
SebahattinDemir <info.sebahattindemir@gmail.com>
2023-08-16 13:42:57 +0300

    Contact_List_Finished

--- src/components/Contacts/Form/index.js
+++ src/components/Contacts/Form/index.js
@@ -1,19 +1,41 @@
-import React from 'react'
+import { useState } from 'react'
 
-function Form() {
-  return (
-    <div>
-     <div>
-        <input name="fullname" placeholder="Full Name" />
-     </div>
-     <div>
-        <input name="phone_number" placeholder="Phone Number" />
-     </div>
-     <div>
-        <button>Add</button>
-     </div>
-    </div>
-  )
+const initialFormValue = { fullname: "", phone_number: "" };
+
+function Form({ addContact, contacts }) {
+   
+   const [form, setForm] = useState(initialFormValue);
+
+   const onChangeInput = (e) => {
+      setForm({ ...form, [e.target.name]: e.target.value });
+   };
+
+   const onSubmit = (e) => {
+      e.preventDefault();
+
+      if (form.fullname === "" || form.phone_number === "") {
+         return false;
+      }
+
+      addContact([...contacts, form]);
+
+      setForm(initialFormValue);
+
+   };
+
+   return (
+      <form onSubmit={onSubmit}>
+         <div>
+            <input name="fullname" placeholder="Full Name" value={form.fullname} onChange={onChangeInput} />
+         </div>
+         <div>
+            <input name="phone_number" placeholder="Phone Number" value={form.phone_number} onChange={onChangeInput} />
+         </div>
+         <div className='btn'>
+            <button>Add</button>
+         </div>
+      </form>
+   )
 }
 
 export default Form
